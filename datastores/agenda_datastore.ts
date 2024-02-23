395d474b756eaab31c27dc412f57c551f6f34b95
unknown <charansrinivasan123@gmail.com>
2024-02-23 00:21:45 -0500

    add datastores for action list and agenda

---
+++ datastores/agenda_datastore.ts
@@ -0,0 +1,29 @@
+import { DefineDatastore, Schema } from "deno-slack-sdk/mod.ts";
+
+const AgendaDatastore = DefineDatastore({
+    name: "Agenda",
+    primary_key: "id",
+    attributes: {
+        id: {
+        type: Schema.types.string,
+      },
+      meeting_date: {
+        type: Schema.slack.types.date,
+      },
+      topics: {
+        items: {
+            type: Schema.types.string,
+        },
+      },
+      topics_status: {
+        items: {
+            type: Schema.types.string,
+        },
+      },
+      isRecurring: {
+         type: Schema.types.boolean 
+        },
+    },
+});
+
+export default AgendaDatastore;
