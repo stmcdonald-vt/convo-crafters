0db86f7ec47f8696eff50f2246261bf8c8587950
unknown <charansrinivasan123@gmail.com>
2024-02-23 21:35:20 -0500

    fix formatting

--- datastores/action_list_datastore.ts
+++ datastores/action_list_datastore.ts
@@ -1,28 +1,28 @@
 import { DefineDatastore, Schema } from "deno-slack-sdk/mod.ts";
 
 const ActionListDatastore = DefineDatastore({
-    name: "ActionList",
-    primary_key: "id",
-    attributes: {
-        id: {
-        type: Schema.types.string,
-      },
-      assigned_to: {
-        type: Schema.slack.types.user_id,
-      },
-      action: {
-        type: Schema.types.string,
-      },
-      status: {
-        type: Schema.types.string,
-      },
-      start_date: {
-        type: Schema.slack.types.date,
-      },
-      end_date: {
-        type: Schema.slack.types.date,
-      },
+  name: "ActionList",
+  primary_key: "id",
+  attributes: {
+    id: {
+      type: Schema.types.string,
     },
+    assigned_to: {
+      type: Schema.slack.types.user_id,
+    },
+    action: {
+      type: Schema.types.string,
+    },
+    status: {
+      type: Schema.types.string,
+    },
+    start_date: {
+      type: Schema.slack.types.date,
+    },
+    end_date: {
+      type: Schema.slack.types.date,
+    },
+  },
 });
 
 export default ActionListDatastore;
