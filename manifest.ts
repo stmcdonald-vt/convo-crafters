607df1ec46b04004cd747724290e757f1d3c32f0
Colby Wentlandt <colbywentlandt@gmail.com>
2024-03-14 17:21:46 -0400

    refactor and format code

--- manifest.ts
+++ manifest.ts
@@ -17,40 +17,45 @@ import { CreatePoll } from "./workflows/create_poll";
  * https://api.slack.com/automation/manifest
  */
 export default Manifest({
-    // This is the app's internal name.
-    name: "Convo-Crafter",
+  // This is the app's internal name.
+  name: "Convo-Crafter",
 
-    // App description the helps users decide whether to us it.
-    description: "A Slack bot that facilitates meeting functions and flow",
+  // App description the helps users decide whether to us it.
+  description: "A Slack bot that facilitates meeting functions and flow",
 
-    // The app's profile picture that will appear in the Slack client.
-    icon: "assets/robot_logo.png",
+  // The app's profile picture that will appear in the Slack client.
+  icon: "assets/robot_logo.png",
 
-    // A list of all workflows the app will use.
-    workflows: [CreateAgenda, CreateMeeting, CreatePoll, CreateReminder],
+  // A list of all workflows the app will use.
+  workflows: [CreateAgenda, CreateMeeting, CreatePoll, CreateReminder],
 
-    // If your app communicates to any external domains, list them here.
-    outgoingDomains: [],
+  // If your app communicates to any external domains, list them here.
+  outgoingDomains: [],
 
-    // A list of all Datastores the app will use
-    datastores: [SampleObjectDatastore, ActionListDatastore, AgendaDatastore, RemindersDatastore],
+  // A list of all Datastores the app will use
+  datastores: [
+    SampleObjectDatastore,
+    ActionListDatastore,
+    AgendaDatastore,
+    RemindersDatastore,
+  ],
 
-    /**
-     * Defines the scope of Convo Crafter's permissions/abilities.
-     * View the following for more info:
-     * https://api.slack.com/scopes?filter=granular_bot
-     */
-    botScopes: [
-        "calls:read", // View information about ongoing and past calls
-        "calls:write", // Start and manage calls in a workspace
-        "commands", // Add shortcuts and/or slash commands that people can use
-        "channels:read", // View basic information about public channels in a workspace
-        "chat:write", // Post messages in approved channels & conversations
-        "chat:write.public", // Send messages to channels @your_slack_app isn't a member of
-        "chat:write.customize", // Send messages as @your_slack_app
-        "datastore:read",
-        "datastore:write",
-        "triggers:write", // Create new Platform triggers
-        "triggers:read", // Read new Platform triggers
-    ],
+  /**
+   * Defines the scope of Convo Crafter's permissions/abilities.
+   * View the following for more info:
+   * https://api.slack.com/scopes?filter=granular_bot
+   */
+  botScopes: [
+    "calls:read", // View information about ongoing and past calls
+    "calls:write", // Start and manage calls in a workspace
+    "commands", // Add shortcuts and/or slash commands that people can use
+    "channels:read", // View basic information about public channels in a workspace
+    "chat:write", // Post messages in approved channels & conversations
+    "chat:write.public", // Send messages to channels @your_slack_app isn't a member of
+    "chat:write.customize", // Send messages as @your_slack_app
+    "datastore:read",
+    "datastore:write",
+    "triggers:write", // Create new Platform triggers
+    "triggers:read", // Read new Platform triggers
+  ],
 });
--- triggers/create_reminder_trigger.ts
+++ triggers/create_reminder_trigger.ts
@@ -5,20 +5,19 @@ import { TriggerContextData, TriggerTypes } from "deno-slack-api/mod.ts";
 /**
  * This link trigger prompts the CreateReminderWorkflow workflow.
  */
-const createReminderTrigger: Trigger<typeof CreateReminder.definition> =
-    {
-        type: TriggerTypes.Shortcut,
-        name: "Setup a Meeting Reminder",
-        description: "Creates a meeting reminder for a given channel.",
-        workflow: `#/workflows/${CreateReminder.definition.callback_id}`,
-        inputs: {
-            interactivity: {
-                value: TriggerContextData.Shortcut.interactivity,
-            },
-            channel: {
-                value: TriggerContextData.Shortcut.channel_id,
-            },
-        },
-    };
+const createReminderTrigger: Trigger<typeof CreateReminder.definition> = {
+  type: TriggerTypes.Shortcut,
+  name: "Setup a Meeting Reminder",
+  description: "Creates a meeting reminder for a given channel.",
+  workflow: `#/workflows/${CreateReminder.definition.callback_id}`,
+  inputs: {
+    interactivity: {
+      value: TriggerContextData.Shortcut.interactivity,
+    },
+    channel: {
+      value: TriggerContextData.Shortcut.channel_id,
+    },
+  },
+};
 
 export default createReminderTrigger;
