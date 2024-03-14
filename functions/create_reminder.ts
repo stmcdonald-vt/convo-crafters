4e6a8845837141bcf26ba610eae11f8392812ab5
Colby Wentlandt <colbywentlandt@gmail.com>
2024-03-14 16:50:00 -0400

    refactor and format code

--- functions/create_reminder.ts
+++ functions/create_reminder.ts
@@ -1,7 +1,7 @@
 import { DefineFunction, Schema, SlackFunction } from "deno-slack-sdk/mod.ts";
 import { RemindersDatastore } from "../datastores/reminders.ts";
 import { SlackAPIClient } from "deno-slack-sdk/types.ts";
-import { CreateReminderWorkflow } from "../workflows/create_reminder.ts";
+import { CreateReminder } from "../workflows/create_reminder.ts";
 
 export const CreateReminderSetupFunction = DefineFunction({
   callback_id: "create_reminder_setup_function",
@@ -70,7 +70,7 @@ export async function findMeetingReminderTrigger(
   }
   const joinedTriggers = allTriggers.triggers.filter((trigger) => (
     trigger.workflow.callback_id ===
-      CreateReminderWorkflow.definition.callback_id &&
+      CreateReminder.definition.callback_id &&
     trigger.channel_ids.includes(channel)
   ));
 
