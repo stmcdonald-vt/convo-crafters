d3fac2343cc39166f61b1a7835cb45201c57676a
Colby Wentlandt <colbywentlandt@gmail.com>
2024-03-21 16:46:16 -0400

    Send request functionality

--- triggers/topic_trigger.ts
+++ triggers/topic_trigger.ts
@@ -1,11 +1,12 @@
 import { Trigger } from "deno-slack-sdk/types.ts";
 import { TriggerContextData, TriggerTypes } from "deno-slack-api/mod.ts";
+import RequestNextTopic from "../workflows/next_topic.ts";
 
-const nextTopicTrigger: Trigger = {
+const nextTopicTrigger: Trigger<typeof RequestNextTopic.definition> = {
   type: TriggerTypes.Shortcut,
   name: "Request to Move on to Next Topic",
   description: "Ask the current speaker to move on to the next topic",
-  workflow: "#/workflows/request_next_topic_workflow.ts",
+  workflow: `#/workflows/${RequestNextTopic.definition.callback_id}`,
   inputs: {
     interactivity: {
       value: TriggerContextData.Shortcut.interactivity,
